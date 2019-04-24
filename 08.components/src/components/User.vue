0346acb60fecd1d81b2b260ed6f506b3a6253255
Sorin <sorin21us@yahoo.com>
2019-04-24 15:08:09 +0300

    Finished 8: emit and receive an event, event bus

---
+++ 08.components/src/components/User.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change name</button>
+    <p>Name is {{name}}</p>
+    <p>Age is {{age}}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <app-user-detail
+          :myName="name"
+          @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"
+        ></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <app-user-edit :userAge="age" @ageWasEdited="age = $event" :changeAgain="changeAgain"></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data() {
+    return {
+      name: "Sorinel",
+      age: 39
+    };
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  },
+  methods: {
+    changeName() {
+      this.name = "Cristina";
+    },
+    resetName() {
+      this.name = "Mike";
+    },
+    changeAgain() {
+      this.age = 17;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ 08.components/src/components/UserEdit.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="component">
+    <h3>You may edit the User here</h3>
+    <p>Edit me!</p>
+    <p>User Age {{userAge}}</p>
+    <button @click="editAge">Edit Age</button>
+    <button @click="changeAgain()">Reset Again</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main.js";
+
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    userAge: Number,
+    changeAgain: Function
+  },
+  methods: {
+    editAge() {
+      this.userAge = 20;
+      // this.$emit("ageWasEdited", this.userAge);
+      // eventBus.$emit("ageWasEdited", this.userAge);
+      eventBus.changeAgeMain(this.userAge);
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightgreen;
+}
+</style>
