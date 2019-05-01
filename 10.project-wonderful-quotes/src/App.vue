07fd0506e72178b1b55a01a2908f38ddf389cd02
Sorin <sorin21us@yahoo.com>
2019-05-01 14:52:14 +0300

    Finish 10: Quotes project

--- 10.project-wonderful-quotes/src/App.vue
+++ 10.project-wonderful-quotes/src/App.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></Header>
+    <NewQuote @newQuote="newQuote"></NewQuote>
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuoteHere"></QuoteGrid>
+    <div class="row">
+      <div class="col-sm-12" text-center>
+        <div class="alert alert-info">Info: click on a quote to delete it</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
 export default {
   data() {
     return {
       quotes: ["Just a simple quote to see something"],
       maxQuotes: 10
     };
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    Header
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        // alert("Delete a quote to add another");
+        return;
+      } else {
+        this.quotes.push(quote);
+      }
+    },
+    deleteQuoteHere(index) {
+      // start splice from index and delete 1 element
+      this.quotes.splice(index, 1);
+    }
   }
 };
 </script>
--- 10.project-wonderful-quotes/src/components/QuoteGrid.vue
+++ 10.project-wonderful-quotes/src/components/QuoteGrid.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="row"></div>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="quote"
+      @click.native="deleteQuote(index)"
+    >{{quote}}</app-quote>
+  </div>
 </template>
 
 <script>
+import Quote from "./Quote";
 export default {
   props: {
     quotes: Array
+  },
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
   }
 };
 </script>
